b23a8625698ffc47d99dfc464c4644de81cafdb4
AlisonOsnir <alison_osnir@hotmail.com>
2022-09-13 02:00:58 -0300

    Second commit

--- script.js
+++ script.js
@@ -4,42 +4,51 @@ const equal = document.querySelector('.equal')
 const numbers = document.querySelectorAll('.numbers div')
 const operators = document.querySelectorAll('.operators div')
 
-let setOperator = false
-let first = 0 
-let second = 0
+let setOperator
+let first
+let second
+
+input.innerText
 
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
-        input.innerText += number.innerText //error 
+        
         if (!setOperator){
-            console.log('first: ',first)
+            input.innerText += number.innerText //error 
+            console.log(number.innerText)
             first = +input.innerText
+            console.log('first: ',first)
         } else {
+            input.innerText += number.innerText //error 
+            console.log(number.innerText)
+            second = +input.innerText  
             console.log('second: ',second)
-            second = +input.innerText
-            setOperator = false
         }
+        if(number.innerText == 'C') clearInputs()
     })
 })
 
 operators.forEach((operator) => {
     operator.addEventListener('click', () => {
-        console.log(operator.innerText)
-        setOperator = true
-        input.innerText = ''
+        setOperator = operator.innerText
+        console.log('setOperator: ', setOperator)
     })
 })
 
-clear.addEventListener('click', () => {
-    input.innerText = ''
-    first = 0
-    second = 0
-})
-
 equal.addEventListener('click', () => {
-    input.innerText = soma()
+    input.innerText = ''
+    if( setOperator == '+') input.innerText = soma()
+    console.log(soma)
+    setOperator = undefined
 })
 
 function soma(){
     return first + second
+}
+
+function clearInputs() {
+    console.log('clear')
+    input.innerText = 0
+    first = 0
+    second = 0
 }
