bc186e856a4f3035ead7a5856c178ac34d99ccd9
Alison Osnir <alison.osnir@constanta.local>
2022-09-13 17:17:34 -0300

    Só por Desus

--- script.js
+++ script.js
@@ -1,54 +1,91 @@
-const clear = document.getElementById('clear')
+const clearBtn = document.getElementById('clear')
 const input = document.querySelector('.input')
-const equal = document.querySelector('.equal')
+const equalBtn = document.querySelector('.equal')
 const numbers = document.querySelectorAll('.numbers div')
 const operators = document.querySelectorAll('.operators div')
 
-let setOperator
-let first
-let second
+const auxiliar = document.querySelector('.auxiliar')
 
-input.innerText
+let setOperator
+let n1
+let n2
+
+let reset = false
 
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
-        
-        if (!setOperator){
-            input.innerText += number.innerText //error 
-            console.log(number.innerText)
-            first = +input.innerText
-            console.log('first: ',first)
+        const num = number.innerText
+
+        if (input.innerText === '0' && num !== '.') input.innerText = '';
+
+        if (!setOperator) {
+            input.innerText += num
+            n1 = +input.innerText
+            
+            auxiliar.innerText = '' //Necessario???
+            auxiliar.innerText += n1
         } else {
-            input.innerText += number.innerText //error 
-            console.log(number.innerText)
-            second = +input.innerText  
-            console.log('second: ',second)
+            if (reset) input.innerText = ''
+            reset = false;
+
+            input.innerText += num
+            n2 = +input.innerText
+            auxiliar.innerText += num
         }
-        if(number.innerText == 'C') clearInputs()
+
+        if (number.innerText === 'C') clear()
+
+        console.log('first: ', n1)
+        console.log('second: ', n2)
     })
 })
 
 operators.forEach((operator) => {
     operator.addEventListener('click', () => {
         setOperator = operator.innerText
-        console.log('setOperator: ', setOperator)
+ 
+        reset = true
+        auxiliar.innerText += setOperator //Teste 
+        console.log('Operador: ', setOperator)
     })
 })
 
-equal.addEventListener('click', () => {
-    input.innerText = ''
-    if( setOperator == '+') input.innerText = soma()
-    console.log(soma)
-    setOperator = undefined
-})
+equalBtn.addEventListener('click', () => equal())
 
-function soma(){
-    return first + second
+function soma() {
+    return n1 + n2
+}
+function subtracao() {
+    return n1 - n2
+}
+function multiplicacao() {
+    return n1 * n2
+}
+function divisao() {
+    return n1 / n2
 }
 
-function clearInputs() {
-    console.log('clear')
+function clear() {
+    n1 = undefined
+    n2 = undefined
     input.innerText = 0
-    first = 0
-    second = 0
+    auxiliar.innerText = ''
+}
+
+function equal() {
+    let result
+
+    if (n1 && n2){
+        input.innerText = ''
+        if (setOperator == '+') result = soma()
+        if (setOperator == '-') result = subtracao()
+        if (setOperator == 'x') result = multiplicacao()
+        if (setOperator == '/') result = divisao()
+
+        input.innerText = result
+        n1 = result;
+        setOperator = undefined
+        auxiliar.innerText += ('=')
+
+    }
 }
