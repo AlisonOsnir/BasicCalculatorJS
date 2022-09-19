7b651d500dcdd775c0d9ceb16303c264dd48d296
AlisonOsnir <alison_osnir@hotmail.com>
2022-09-19 00:22:18 -0300

    Starting refactoring

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const display = document.querySelector('.display')
-const equalBtn = document.querySelector('.equal')
-const inputs = document.querySelectorAll('.numbers div')
+//  Develope next feature: 
+//    Recriar o funcionmamento da calculadora do celular
+//      Sempre mostrar '='+result no display ( pressionar apenas muda o font-size e weight)
+
+// Add Keyboard use
+
+const displayResult = document.querySelector('.displayResult')
+const displayCalc = document.querySelector('.displayCalc')               //Renomear
+const numbers = document.querySelectorAll('.numbers div')
 const operators = document.querySelectorAll('.operators div')
-const auxiliar = document.querySelector('.auxiliar')               //Renomear
+const equalBtn = document.querySelector('.equal')
+//const clear = document.getElementById('clear')
 
 let num1
 let num2
@@ -10,39 +17,36 @@ let setOperator
 let result
 let resetDisplayToNum2 = false
 
-// Develope next feature: Executar operação precionando proximo operador
-//      if numero de operador > num1 = equal()
-
-// Add Keyboard use
-
 //setNums
-inputs.forEach((inputBtn) => {
-    inputBtn.addEventListener('click', () => {
-        if (display.innerText === '0' && inputBtn.innerText !== '.') display.innerText = '';
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        if (displayResult.innerText === '0' && number.innerText !== '.') {
+            displayResult.innerText = '=';
+        }
         //Clear for next setnum or calc
-        if (resetDisplayToNum2 || result == display.innerText) {
-            display.innerText = ''
+        if (resetDisplayToNum2 || result == displayResult.innerText) {
+            displayResult.innerText = '='
             result = undefined
             resetDisplayToNum2 = false
         }
 
         if (!setOperator) {
-            display.innerText += inputBtn.innerText
-            num1 = parseFloat(display.innerText)        //Tratar das dizimas periodicas
-            auxiliar.innerText = ''
-            auxiliar.innerText += num1
+            displayResult.innerText += number.innerText
+            num1 = parseFloat(displayResult.innerText.slice(1))        //Tratar das dizimas periodicas
+            displayCalc.innerText = ''
+            displayCalc.innerText += num1
         } else {
             //Add zero before dot
-            if (display.innerText == '' && inputBtn.innerText == '.') {
-                display.innerText = '0'
-                auxiliar.innerText += '0'
+            if (displayResult.innerText == '' && number.innerText == '.') {
+                displayResult.innerText = '=0'
+                displayCalc.innerText += '0'
             }
 
-            display.innerText += inputBtn.innerText
-            num2 = parseFloat(display.innerText)
-            auxiliar.innerText += inputBtn.innerText
+            displayResult.innerText += number.innerText
+            num2 = parseFloat(displayResult.innerText)
+            displayCalc.innerText += number.innerText
         }
-        if (inputBtn.innerText === 'C') clear()         //SEPARAR LISTENER POR ID?
+        if (number.innerText === 'C') clear()         //SEPARAR LISTENER POR ID?
     })
 })
 
@@ -51,28 +55,20 @@ operators.forEach((operator) => {
     operator.addEventListener('click', () => {
         //Show result of the last calc
         if (result) {
-            auxiliar.innerText = result
+            displayCalc.innerText = result
         }
         if (num1) {
             setOperator = operator.innerText
             resetDisplayToNum2 = true
-            auxiliar.innerText += setOperator
+            displayCalc.innerText += setOperator
         }
     })
 })
 
-function soma() {
-    return num1 + num2
-}
-function subtracao() {
-    return num1 - num2
-}
-function multiplicacao() {
-    return num1 * num2
-}
-function divisao() {
-    return num1 / num2
-}
+function soma() { return num1 + num2 }
+function subtracao() { return num1 - num2 }
+function multiplicacao() { return num1 * num2 }
+function divisao() { return num1 / num2 }
 
 function equal() {
     if (num1, num2) {
@@ -83,8 +79,8 @@ function equal() {
         num1 = result;
         num2 = undefined
         setOperator = undefined
-        display.innerText = result
-        auxiliar.innerText += ('=')
+        displayResult.innerText = result
+        displayCalc.innerText += ('=')
     }
 }
 
@@ -92,8 +88,8 @@ function clear() {
     num1 = undefined
     num2 = undefined
     result = undefined
-    display.innerText = 0
-    auxiliar.innerText = ''
+    displayResult.innerText = 0
+    displayCalc.innerText = ''
     resetDisplayToNum2 = false
 }
 
