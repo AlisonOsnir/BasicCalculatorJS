4a8a276caebca46de72e3b11e7dc071f39d4d6fc
Alison Osnir <alison.osnir@constanta.local>
2022-09-23 15:38:34 -0300

    Update script, stat new script-test

--- script.js
+++ script.js
@@ -1,68 +1,85 @@
-//  Develope next feature: 
-//    Recriar o funcionmamento da calculadora do celular
-//      Sempre mostrar '='+result no display ( pressionar apenas muda o font-size e weight)
-
-// Add Keyboard use
+//  tratar precisão dos decimais
+//  Add Keyboard input
 
 const displayResult = document.querySelector('.displayResult')
-const displayCalc = document.querySelector('.displayCalc')               //Renomear
-const numbers = document.querySelectorAll('.numbers div')
+const displayCalc = document.querySelector('.displayCalc')
 const operators = document.querySelectorAll('.operators div')
+const numbers = document.querySelectorAll('.numbers div')
 const equalBtn = document.querySelector('.equal')
-//const clear = document.getElementById('clear')
+const clearBtn = document.getElementById('clear')
 
-let num1
-let num2
-let setOperator
-let result
-let resetDisplayToNum2 = false
+let num1 = ''
+let num2 = ''
+let result = ''
+let setOperator = ''
+displayCalc.innerText = 0
+let nextCalc = false
+
 
 //setNums
 numbers.forEach((number) => {
-    number.addEventListener('click', () => {
-        if (displayResult.innerText === '0' && number.innerText !== '.') {
-            displayResult.innerText = '=';
-        }
-        //Clear for next setnum or calc
-        if (resetDisplayToNum2 || result == displayResult.innerText) {
-            displayResult.innerText = '='
-            result = undefined
-            resetDisplayToNum2 = false
-        }
+  number.addEventListener('click', () => {
 
-        if (!setOperator) {
-            displayResult.innerText += number.innerText
-            num1 = parseFloat(displayResult.innerText.slice(1))        //Tratar das dizimas periodicas
-            displayCalc.innerText = ''
-            displayCalc.innerText += num1
-        } else {
-            //Add zero before dot
-            if (displayResult.innerText == '' && number.innerText == '.') {
-                displayResult.innerText = '=0'
-                displayCalc.innerText += '0'
-            }
+    displayCalc.classList.add('displayFocus')
+    displayResult.classList.remove('displayFocus')
 
-            displayResult.innerText += number.innerText
-            num2 = parseFloat(displayResult.innerText)
-            displayCalc.innerText += number.innerText
-        }
-        if (number.innerText === 'C') clear()         //SEPARAR LISTENER POR ID?
-    })
+    if (nextCalc) clear()
+
+    if (!setOperator) {
+      // dot handler
+      if (num1 !== '' && number.innerText == '.') {
+        num1 += number.innerText
+        displayCalc.innerText += number.innerText
+        return
+      }
+      if (num1 === '' && number.innerText == '.') {
+        num1 = '0.'
+        displayCalc.innerText = num1
+        return
+      }
+      num1 += number.innerText
+      num1 = parseFloat(num1)
+      displayCalc.innerText = num1
+      displayResult.innerText = ('=' + num1)
+    } else {
+      // dot handler
+      if (num2 !== '' && number.innerText == '.') {
+        num2 += number.innerText
+        displayCalc.innerText += number.innerText
+        return num2
+      }
+      if (num2 === '' && number.innerText == '.') {
+        num2 = '0.'
+        displayCalc.innerText += num2
+        return num2
+      }
+      num2 += number.innerText
+      num2 = parseFloat(num2)
+      displayCalc.innerText = (num1 + setOperator + num2)
+      displayResult.innerText = ('=' + num2)
+    }
+
+    if (num1, num2) {
+      equal()
+    }
+  })
 })
 
 //setOperators
 operators.forEach((operator) => {
-    operator.addEventListener('click', () => {
-        //Show result of the last calc
-        if (result) {
-            displayCalc.innerText = result
-        }
-        if (num1) {
-            setOperator = operator.innerText
-            resetDisplayToNum2 = true
-            displayCalc.innerText += setOperator
-        }
-    })
+  operator.addEventListener('click', () => {
+    //Use result to next calc
+    if (result) {
+      displayCalc.innerText = result
+      num1 = result
+      nextCalc = false
+    }
+    if (num1) {
+      setOperator = operator.innerText
+      num2 = ''
+      displayCalc.innerText += setOperator
+    }
+  })
 })
 
 function soma() { return num1 + num2 }
@@ -71,26 +88,29 @@ function multiplicacao() { return num1 * num2 }
 function divisao() { return num1 / num2 }
 
 function equal() {
-    if (num1, num2) {
-        if (setOperator == '+') result = soma()
-        if (setOperator == '-') result = subtracao()
-        if (setOperator == 'x') result = multiplicacao()
-        if (setOperator == '/') result = divisao()
-        num1 = result;
-        num2 = undefined
-        setOperator = undefined
-        displayResult.innerText = result
-        displayCalc.innerText += ('=')
-    }
+  if (num1, num2) {
+    if (setOperator == '+') result = soma()
+    if (setOperator == '-') result = subtracao()
+    if (setOperator == 'x') result = multiplicacao()
+    if (setOperator == '/') result = divisao()
+    displayResult.innerText = ('=' + result.toPrecision())          //TRATAR PRECISAO DE DIZIMAS 
+  }
 }
 
 function clear() {
-    num1 = undefined
-    num2 = undefined
-    result = undefined
-    displayResult.innerText = 0
-    displayCalc.innerText = ''
-    resetDisplayToNum2 = false
+  num1 = ''
+  num2 = ''
+  result = ''
+  setOperator = ''
+  displayCalc.innerText = '0'
+  displayResult.innerText = ''
+  nextCalc = false
 }
 
-equalBtn.addEventListener('click', () => equal())
+equalBtn.addEventListener('click', () => {
+  displayCalc.classList.remove('displayFocus')
+  displayResult.classList.add('displayFocus')
+  nextCalc = true
+})
+
+clearBtn.onclick = clear
